f690d426a69f78eb997b922041410e3ea7e7be06
4rthurC4str0 <arthur.castro.pessoa@gmail.com>
2025-06-27 15:56:43 -0300

    modificação no exercicio_do_cpf -> foi adicionado verificação de sequencias; tratamento do cpf com pontos e travessão, e adicionado o cálculo do segundo digito

--- exercicio_do_cpf.py
+++ exercicio_do_cpf.py
@@ -1,15 +1,46 @@
 # CPF: 746.824.890-70
+import sys
+cpf_enviado_usuario = '746.824.890-70'\
+    .replace('.', '')\
+    .replace('-', '')\
+    .replace(' ', '')
 
-cpf = '74682489070'
-nove_digitos = cpf[:9]
-print(nove_digitos)
+entrada_e_sequencial = cpf_enviado_usuario == cpf_enviado_usuario[0] * len(cpf_enviado_usuario)
+
+if entrada_e_sequencial == True:
+    print('Você enviou dados sequenciais')
+    sys.exit()
+
+
+
+
+nove_digitos = cpf_enviado_usuario[:9]
 
 contador_regressivo_1 = 10
-digitos_somados = 0
+digitos_somados_1 = 0
 for digito in nove_digitos:
-    digitos_somados += int(digito) * contador_regressivo_1
+    digitos_somados_1 += int(digito) * contador_regressivo_1
     contador_regressivo_1 -= 1
 
-primeiro_digito = (digitos_somados * 10) % 11
+primeiro_digito = (digitos_somados_1 * 10) % 11
 
-primeiro_digito = primeiro_digito if primeiro_digito <= 9 else primeiro_digito = 0
+primeiro_digito = primeiro_digito if primeiro_digito <= 9 else 0
+
+dez_digitos = nove_digitos + str(primeiro_digito)
+
+contador_regressivo_2 = 11
+digitos_somados_2 = 0
+for digito in dez_digitos:
+    digitos_somados_2 += int(digito) * contador_regressivo_2
+    contador_regressivo_2 -= 1
+
+segundo_digito = (digitos_somados_2 * 10) % 11
+
+segundo_digito = segundo_digito if segundo_digito <= 9 else 0
+
+cpf_gerado_calculo = f'{nove_digitos}{primeiro_digito}{segundo_digito}'
+
+if cpf_enviado_usuario == cpf_gerado_calculo:
+    print(f'{cpf_enviado_usuario} é válido!')
+else:
+    print(f'{cpf_enviado_usuario} não é válido')
